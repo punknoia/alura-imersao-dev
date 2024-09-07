6875af1f815b23c62dfe4682b083c5bc86e07448
Daniel Borges <damborges@gmail.com>
2024-09-06 23:00:35 -0300

    ultima aula

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("Olá Mundo");
+function pesquisar() {
+
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Você não digitou nada</p>";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      resultados += `
+      <div class="item-resultado">
+        <h2>${dado.titulo}</h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href="${dado.link}" target="_blank">Site Oficial</a>
+      </div>
+    `;
+    }
+    if (!resultados) {
+      resultados = "<p>Nada foi encontrado</p>";
+    }   
+  }
+
+  section.innerHTML = resultados;
+}
+
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+  {
+    titulo: "Star Trek",
+    descricao:
+      "Star Trek é uma icônica franquia de ficção científica que nos transporta para um futuro onde a humanidade explora a galáxia a bordo de espaçonaves avançadas. Através das aventuras da USS Enterprise e de suas tripulações, Star Trek nos convida a refletir sobre a condição humana, a ética e o nosso lugar no universo.",
+    link: "https://www.instagram.com/startrek/",
+    tags: "kirk spock jornada"
+  },
+  {
+    titulo: "Stranger Things",
+    descricao:
+      "Uma mistura perfeita de nostalgia dos anos 80, suspense e elementos sobrenaturais. A série acompanha um grupo de amigos que, após o desaparecimento de um deles, se deparam com forças misteriosas e uma dimensão paralela.",
+    link: "https://www.instagram.com/strangerthingstv/",
+    tags: "monstro invertido hawkings"
+  },
+  {
+    titulo: "Black Mirror",
+    descricao:
+      "Uma antologia que explora as consequências da tecnologia na sociedade, apresentando histórias independentes e perturbadoras que te farão questionar o futuro da humanidade.",
+    link: "https://www.netflix.com/br/title/70264888",
+    tags: "smartphone utopia"
+  },
+];
